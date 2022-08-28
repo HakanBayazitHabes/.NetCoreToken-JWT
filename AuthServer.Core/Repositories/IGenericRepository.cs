3ed52563ccf1480d88614d646a8db9ec48031710
Hakan Bayazit Habes <hakanbayazithabes@gmail.com>
2022-08-28 08:21:40 +0300

    Creating the IRepository interface

---
+++ AuthServer.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Repositories
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        Task<TEntity> GetByIdAsync(int id);
+        Task<IEnumerable<TEntity>> GetAllAsync(); //GetAllAsync içerisinden gelen veriler üzerinden where işlemi gerçekleştirelemz.Direkt olarak ToList() halinde gelir
+        IQueryable<TEntity> Where(Expression<Func<TEntity, bool>> predicate);
+
+        Task AddAsync(TEntity entity);
+        void Remove(TEntity entity);        
+
+    }
+}
