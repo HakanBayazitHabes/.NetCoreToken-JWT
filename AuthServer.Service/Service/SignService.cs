da9794341cf99d82b26784ea9f0a698d47161a28
Hakan Bayazit Habes <hakanbayazithabes@gmail.com>
2022-09-01 00:26:37 +0300

    Creating the SignService class

---
+++ AuthServer.Service/Service/SignService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Service.Service    
+{
+    internal static class SignService
+    {
+        public static SecurityKey GetSymetricSecuritySecurityKey(string securitKey)
+        {
+            return new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securitKey));
+        } 
+    }
+}
